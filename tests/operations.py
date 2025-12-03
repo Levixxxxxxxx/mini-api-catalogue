61d76a9be51b9abd0806ceaef12984d4304cf257
YanisBra <yanis.brahimi91@gmail.com>
2025-12-03 14:19:22 +0100

    ajout des fonctions pyhton ainsi que de leurs tests

---
+++ tests/operations.py
@@ -0,0 +1,8 @@
+def addition(a, b):
+    return a + b
+
+def maximum(a, b):
+    return a if a > b else b
+
+def format_name(name):
+    return name.strip().title()
---
+++ tests/test.py
@@ -0,0 +1,8 @@
+from operations import addition, maximum, format_name
+
+assert addition(2, 3) == 5
+
+assert maximum(10, 5) == 10
+
+assert format_name("john") == "John"
+
